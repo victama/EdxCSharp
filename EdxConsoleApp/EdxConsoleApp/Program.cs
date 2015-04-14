b69f6a0338f436946ff550a07c2795231cdeaa4b
victama <victortamames@gmail.com>
2015-04-14 21:18:35 +0200

    module 2 refactor

--- EdxConsoleApp/EdxConsoleApp/Program.cs
+++ EdxConsoleApp/EdxConsoleApp/Program.cs
@@ -10,126 +10,199 @@ namespace EdxConsoleApplication
     {
         static void Main(string[] args)
         {
-            // Student information, such as: First Name, Last Name, Birthdate, Address Line 1 and 2, City, State/Province, Zip/Postal, Country, etc...
-            string studentFirstName;
-            string studentLastName;
-            DateTime studentBirthdate;
-            string studentAddressLine1;
-            string studentAddressLine2;
-            string studentCity;
-            string studentStateOrProvince;
-            string studentZipCode;
-            string studentCountry;
-            int studentTelephoneNumber;
-            string studentAwards;
-            string studentNotes;
+            GetStudentInfo();
+            GetProfessorInfo();
+            GetCourseInfo();
+            GetDegreeInfo();
+            GetProgramInfo();
 
-            // Professor information with pertinent fields
-            string professorFirstName;
-            string professorLastName;
-            DateTime professorBirthdate;
-            string professorAddressLine1;
-            string professorAddressLine2;
-            string professorCity;
-            string professorStateOrProvince;
-            string professorZipCode;
-            string professorCountry;
-            int professorTelephoneNumber;
-            string professorCertifications;
-            string professorNotes;
+            Console.Write("Press any key to close this window...");
+            Console.ReadKey();
 
-            // A university degree with pertinent fields
-            string degreeName;
-            string degreeDescription;
-            int degreeCredits;
+            ValidateBirthDate("");
+        }
 
-            // A university program with pertinent fields
-            string programName;
-            string programDegrees;
-            string programHeadDepartment;
+        static void GetStudentInfo()
+        {
+            // Student information, such as: First Name, Last Name, Birthdate, Address, City, State/Province, Zip/Postal, Country
+            string firstName;
+            string lastName;
+            DateTime birthdate;
+            string address;
+            string city;
+            string stateOrProvince;
+            string zipCode;
+            string country;
 
-            // Information for a course that would be part of your selected degree and program, with pertinent fields
-            string courseName;
-            string courseDescription;
-            int courseCredits;
-            bool courseHasStarted;
-            string courseDegree;
+            Console.WriteLine("Enter the student's first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Enter the student's birth date: ");
+            string birthdateStr = Console.ReadLine();
+            birthdate = DateTime.Parse(birthdateStr);
+            Console.WriteLine("Enter the student's address: ");
+            address = Console.ReadLine();
+            Console.WriteLine("Enter the student's city: ");
+            city = Console.ReadLine();
+            Console.WriteLine("Enter the student's state/province: ");
+            stateOrProvince = Console.ReadLine();
+            Console.WriteLine("Enter the student's zip code: ");
+            zipCode = Console.ReadLine();
+            Console.WriteLine("Enter the student's country: ");
+            country = Console.ReadLine();
 
-            // ASSIGN DATA
-            studentFirstName = "Luis";
-            studentLastName = "Gutierrez";
-            studentBirthdate = DateTime.Parse("1982/12/12");
-            studentAddressLine1 = "5C, 46, Mieses Street";
-            studentAddressLine2 = "";
-            studentCity = "Cadiz";
-            studentStateOrProvince = "Andalucia";
-            studentZipCode = "11111";
-            studentCountry = "Spain";
-            studentTelephoneNumber = 654987321;
-            studentAwards = "Promotion's first";
-            studentNotes = "Smooth criminal";
+            PrintStudentDetails(firstName, lastName, address, zipCode, city, stateOrProvince, country, birthdate);
+        }
 
-            professorFirstName = "Valeria";
-            professorLastName = "Mazza";
-            professorBirthdate = DateTime.Parse("1966/06/06");
-            professorAddressLine1 = "128, Huxley Avenue";
-            professorAddressLine2 = "";
-            professorCity = "London";
-            professorStateOrProvince = "London";
-            professorZipCode = "W12";
-            professorCountry = "United Kingdom";
-            professorTelephoneNumber = 645978312;
-            professorCertifications = "Master of Puppets";
-            professorNotes = "Son of the Great Britain";
-
-            degreeName = "Bachelor of Science in Information Technology";
-            degreeDescription = "The curriculum for Baker’s Computer Science program is based on guidance from industry professionals and updated continuously to prepare you for any one of a number of careers in today’s workplaces.";
-            degreeCredits = 240;
-
-            programName = "Computer Science or Arts";
-            programDegrees = "Bachelor, Master, Ph.D.";
-            programHeadDepartment = "Valeria Mazza";
-
-            courseName = "Computer Science 101";
-            courseDescription = "CS101 teaches the essential ideas of Computer Science for a zero-prior-experience audience. The course uses small coding experiments in the browser to play with the nature of computers, understanding their strengths and limitations. Sign up for the 'To be announced' session to be notified by email when the class is next run, and sign up for 'Self-Study' to start browsing the class materials right away. Self-Study mode makes all the videos and assignments available to be done at your own pace, but without a certificate of completion at the end.";
-            courseCredits = 40;
-            courseDegree = "Bachelor of Science in Information Technology";
-            courseHasStarted = true;
-
-            // PRINT DATA
+        static void PrintStudentDetails(string first, string last, string address, string zipCode, string city, string stateOrProvince, string country, DateTime birthday)
+        {
             Console.WriteLine();
             Console.WriteLine("Student information:");
-            Console.WriteLine("Full name: {0} {1}", studentFirstName, studentLastName);
-            Console.WriteLine("Full address: {0} {1}, {2}, {3}, {4}, {5}", studentAddressLine1, studentAddressLine2, studentZipCode, studentCity, studentStateOrProvince, studentCountry);
-            Console.WriteLine("Birthdate: {0}, Telephone: {1}, Awards: {2}, Notes: {3}", studentBirthdate, studentTelephoneNumber, studentAwards, studentNotes);
+            Console.WriteLine("Full name: {0} {1}", first, last);
+            Console.WriteLine("Full address: {0}, {1}, {2}, {3}, {4}", address, zipCode, city, stateOrProvince, country);
+            Console.WriteLine("Birthdate: {0}", birthday);
+            Console.WriteLine();
+        }
 
+        static void GetProfessorInfo()
+        {
+            // Professor information with pertinent fields
+            string firstName;
+            string lastName;
+            DateTime birthdate;
+            string address;
+            string city;
+            string stateOrProvince;
+            string zipCode;
+            string country;
+
+            Console.WriteLine("Enter the professor's first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter the professor's last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Enter the professor's birth date: ");
+            string birthdateStr = Console.ReadLine();
+            birthdate = DateTime.Parse(birthdateStr);
+            Console.WriteLine("Enter the professor's address: ");
+            address = Console.ReadLine();
+            Console.WriteLine("Enter the professor's city: ");
+            city = Console.ReadLine();
+            Console.WriteLine("Enter the professor's state/province: ");
+            stateOrProvince = Console.ReadLine();
+            Console.WriteLine("Enter the professor's zip code: ");
+            zipCode = Console.ReadLine();
+            Console.WriteLine("Enter the professor's country: ");
+            country = Console.ReadLine();
+
+            PrintProfessorDetails(firstName, lastName, address, zipCode, city, stateOrProvince, country, birthdate);
+        }
+
+        static void PrintProfessorDetails(string first, string last, string address, string zipCode, string city, string stateOrProvince, string country, DateTime birthday)
+        {
             Console.WriteLine();
             Console.WriteLine("Professor information:");
-            Console.WriteLine("Full name: {0} {1}", professorFirstName, professorLastName);
-            Console.WriteLine("Full address: {0} {1}, {2}, {3}, {4}, {5}", professorAddressLine1, professorAddressLine2, professorZipCode, professorCity, professorStateOrProvince, professorCountry);
-            Console.WriteLine("Birthdate: {0}, Telephone: {1}, Certifications: {2}, Notes: {3}", professorBirthdate, professorTelephoneNumber, professorCertifications, professorNotes);
-
+            Console.WriteLine("Full name: {0} {1}", first, last);
+            Console.WriteLine("Full address: {0}, {1}, {2}, {3}, {4}", address, zipCode, city, stateOrProvince, country);
+            Console.WriteLine("Birthdate: {0}", birthday);
             Console.WriteLine();
-            Console.WriteLine("Degree information:");
-            Console.WriteLine("Name: {0}", degreeName);
-            Console.WriteLine("Description: {0}", degreeDescription);
-            Console.WriteLine("Credits: {0}", degreeCredits);
+        }
 
-            Console.WriteLine();
-            Console.WriteLine("Program information:");
-            Console.WriteLine("Name: {0}", programName);
-            Console.WriteLine("Degrees: {0}", programDegrees);
-            Console.WriteLine("Department Head: {0}", programHeadDepartment);
+        static void GetCourseInfo()
+        {
+            // Information for a course that would be part of your selected degree and program, with pertinent fields
+            string name;
+            string description;
+            int credits;
+            string degree;
+            bool hasStarted;
 
+            Console.WriteLine("Enter the course's name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the course's description: ");
+            description = Console.ReadLine();
+            Console.WriteLine("Enter the course's credits: ");
+            string creditsStr = Console.ReadLine();
+            credits = Convert.ToInt32(creditsStr);
+            Console.WriteLine("Enter the course's degree: ");
+            degree = Console.ReadLine();
+            Console.WriteLine("Has the course started? [Y/N] ");
+            string hasStartedStr = Console.ReadLine();
+            hasStarted = "Y".Equals(hasStartedStr) ? true : false;
+            
+            PrintCourseDetails(name, description, credits, degree, hasStarted);
+        }
+
+        static void PrintCourseDetails(string name, string description, int credits, string degree, bool hasStarted)
+        {
             Console.WriteLine();
             Console.WriteLine("Course information:");
-            Console.WriteLine("Name: {0}", courseName);
-            Console.WriteLine("Description: {0}", courseDescription);
-            Console.WriteLine("Credits: {0}", courseCredits);
-            Console.WriteLine("Degree: {0}", courseDegree);
-            Console.WriteLine("Has started? {0}", courseHasStarted ? "Yes" : "No");
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Description: {0}", description);
+            Console.WriteLine("Credits: {0}", credits);
+            Console.WriteLine("Degree: {0}", degree);
+            Console.WriteLine("Has started? {0}", hasStarted ? "Yes" : "No");
+            Console.WriteLine();
+        }
 
-            Console.ReadKey();
+        static void GetDegreeInfo()
+        {
+            // A university degree with pertinent fields
+            string name;
+            string description;
+            int credits;
+
+            Console.WriteLine("Enter the degree's name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the degree's description: ");
+            description = Console.ReadLine();
+            Console.WriteLine("Enter the degree's credits: ");
+            string creditsStr = Console.ReadLine();
+            credits = Convert.ToInt32(creditsStr);
+
+            PrintDegreeDetails(name, description, credits);
+        }
+
+        static void PrintDegreeDetails(string name, string description, int credits)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Degree information:");
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Description: {0}", description);
+            Console.WriteLine("Credits: {0}", credits);
+            Console.WriteLine();
+        }
+
+        static void GetProgramInfo()
+        {
+            // A university program with pertinent fields
+            string name;
+            string degrees;
+            string headDepartment;
+
+            Console.WriteLine("Enter the program's name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the program's degrees: ");
+            degrees = Console.ReadLine();
+            Console.WriteLine("Enter the program's head department: ");
+            headDepartment = Console.ReadLine();
+
+            PrintProgramDetails(name, degrees, headDepartment);
+        }
+
+        static void PrintProgramDetails(string name, string degrees, string headDepartment)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Program information:");
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Degrees: {0}", degrees);
+            Console.WriteLine("Department Head: {0}", headDepartment);
+            Console.WriteLine();
+        }
+
+        static void ValidateBirthDate(string birthdateStr)
+        {
+            throw new NotImplementedException();
         }
     }
 }
