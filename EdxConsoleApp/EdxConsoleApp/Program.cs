1989d9c13e90cc10169cf2767ba0488eba3407b8
vtamames <vetamames@optimyth.com>
2015-04-20 17:10:34 +0200

    Using classes

--- EdxConsoleApp/EdxConsoleApp/Program.cs
+++ EdxConsoleApp/EdxConsoleApp/Program.cs
@@ -8,111 +8,166 @@ namespace EdxConsoleApplication
 {
 
     /**
-     * Student information, such as: First Name, Last Name, Birthdate, Address, City, State/Province, Zip/Postal, Country
+     * Student information, such as: First Name, Last Name, Birthdate, Address
      */
-    public struct Student
+    public class Student
     {
-        public string firstName;
-        public string lastName;
-        public DateTime birthdate;
-        public string address;
-        public string city;
-        public string stateOrProvince;
-        public string zipCode;
-        public string country;
+        // public static class variable with number of Student instances
+        public static int Enrolled { get; set; }
 
-        public Student(string firstName, string lastName, DateTime birthdate, string address, string city, string stateOrProvince, string zipCode, string country)
+        // private fields
+        private string firstName;
+        private string lastName;
+        private DateTime birthdate;
+        private string address;
+
+        // properties encapsulating fields
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public DateTime Birthdate { get { return birthdate; } set { birthdate = value; } }
+        public string Address { get { return address; } set { address = value; } }
+
+        public Student(string firstName, string lastName, DateTime birthdate, string address)
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.birthdate = birthdate;
-            this.address = address;
-            this.city = city;
-            this.stateOrProvince = stateOrProvince;
-            this.zipCode = zipCode;
-            this.country = country;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Birthdate = birthdate;
+            this.Address = address;
+            Enrolled++;
         }
     }
 
     /**
-     * Professor information with pertinent fields
+     * Teacher information with pertinent fields
      */
-    public struct Professor
+    public class Teacher
     {
-        public string firstName;
-        public string lastName;
-        public DateTime birthdate;
-        public string address;
-        public string city;
-        public string stateOrProvince;
-        public string zipCode;
-        public string country;
+        // private fields
+        private string firstName;
+        private string lastName;
+        private DateTime birthdate;
+        private string address;
 
-        public Professor(string firstName, string lastName, DateTime birthdate, string address, string city, string stateOrProvince, string zipCode, string country)
+        // properties encapsulating fields
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public DateTime Birthdate { get { return birthdate; } set { birthdate = value; } }
+        public string Address { get { return address; } set { address = value; } }
+
+        public Teacher(string firstName, string lastName, DateTime birthdate, string address)
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.birthdate = birthdate;
-            this.address = address;
-            this.city = city;
-            this.stateOrProvince = stateOrProvince;
-            this.zipCode = zipCode;
-            this.country = country;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Birthdate = birthdate;
+            this.Address = address;
         }
     }
 
     /**
      * Information for a course that would be part of your selected degree and program, with pertinent fields
      */
-    public struct Course
+    public class Course
     {
-        public string name;
-        public string description;
-        public int credits;
-        public string degree;
-        public bool hasStarted;
+        // private fields
+        private string name;
+        private string description;
+        private int credits;
+        private Degree degree;
+        private bool hasStarted;
 
-        public Course(string name, string description, int credits, string degree, bool hasStarted)
+        private Student[] students;
+        private Teacher[] teachers;
+
+        // properties encapsulating fields
+        public string Name { get { return name; } set { name = value; } }
+        public string Description { get { return description; } set { description = value; } }
+        public int Credits { get { return credits; } set { credits = value; } }
+        public Degree Degree { get { return degree; } set { degree = value; } }
+        public bool HasStarted { get { return hasStarted; } set { hasStarted = value; } }
+
+        public Course(string name, string description, int credits, bool hasStarted)
         {
-            this.name = name;
-            this.description = description;
-            this.credits = credits;
-            this.degree = degree;
-            this.hasStarted = hasStarted;
+            this.Name = name;
+            this.Description = description;
+            this.Credits = credits;
+            this.HasStarted = hasStarted;
+        }
+
+        public bool AddStudent(Student student)
+        {
+            if (student == null) throw new ArgumentNullException();
+            if (students == null) students = new Student[3];
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i] == null)
+                {
+                    students[i] = student;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool AddTeacher(Teacher teacher)
+        {
+            if (teacher == null) throw new ArgumentNullException();
+            if (teachers == null) teachers = new Teacher[3];
+            for (int i = 0; i < teachers.Length; i++)
+            {
+                if (teachers[i] == null)
+                {
+                    teachers[i] = teacher;
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
     /**
      * A university degree with pertinent fields
      */
-    public struct Degree
+    public class Degree
     {
-        public string name;
-        public string description;
-        public int credits;
+        // private fields
+        private string name;
+        private string description;
+        private int credits;
+        private Course course;
+
+        // properties encapsulating fields
+        public string Name { get { return name; } set { name = value; } }
+        public string Description { get { return description; } set { description = value; } }
+        public int Credits { get { return credits; } set { credits = value; } }
+        public Course Course { get { return course; } set { course = value; } }
 
         public Degree(string name, string description, int credits)
         {
-            this.name = name;
-            this.description = description;
-            this.credits = credits;
+            this.Name = name;
+            this.Description = description;
+            this.Credits = credits;
         }
     }
 
     /**
      * A university program with pertinent fields
      */
-    public struct UniversityProgram
+    public class UProgram
     {
-        public string name;
-        public string degrees;
-        public string headDepartment;
+        // private fields
+        private string name;
+        private Teacher headDepartment;
+        private Degree degree;
 
-        public UniversityProgram(string name, string degrees, string headDepartment)
+        // properties encapsulating fields
+        public string Name { get { return name; } set { name = value; } }
+        public Degree Degree { get { return degree; } set { degree = value; } }
+        public Teacher HeadDepartment { get { return headDepartment; } set { headDepartment = value; } }
+
+        public UProgram(string name, Teacher headDepartment)
         {
-            this.name = name;
-            this.degrees = degrees;
-            this.headDepartment = headDepartment;
+            this.Name = name;
+            this.HeadDepartment = headDepartment;
         }
     }
 
@@ -121,36 +176,46 @@ namespace EdxConsoleApplication
         static void Main(string[] args)
         {
             // Variable definition
-            Student[] students;
+            Teacher teacher;
+            Course course;
+            Degree degree;
+            UProgram program;
 
-            // Var initialization
-            students = new Student[5];
-
-            // Students info initialization (in loop, same data for all of them, but using loop index to distinguish)
-            for (int i = 0; i < 5; i++)
+            // Variable initialization
+            course = new Course("Programming with C#", "Let's learn to program in C# with M$!", 40, true);
+            for (int i = 0; i < 3; i++)
             {
                 int humanCardinal = i + 1;
-                students[i] = new Student("Student " + humanCardinal, "Fullname", new DateTime(1982, 12, humanCardinal), "5C, 46, Mieses Street", "Cadiz", "Andalucia", "11111", "Spain");
+                Student student = new Student("Student " + humanCardinal, "Fullname", new DateTime(1982, 12, humanCardinal), "5C, 46, Mieses Street, Cadiz, Spain");
+                course.AddStudent(student);
             }
+            teacher = new Teacher("Valeria", "Mazza", new DateTime(1972, 2, 17), "69, Calle del Rosario, Santa Fe, Argentina");
+            course.AddTeacher(teacher);
+            degree = new Degree("Bachelor of Science", "Bachelor's degree for this exercise", 240);
+            degree.Course = course;
+            course.Degree = degree;
+            program = new UProgram("Information Technology", teacher);
+            program.Degree = degree;
 
-            // Students info visualization in console
-            for (int i = 0; i < 5; i++)
-            {
-                PrintStudentDetails(students[i]);
-            }
+            // Using Console.WriteLine statements, output the following information to the console window:
+            // - The name of the program and the degree it contains
+            // - The name of the course in the degree
+            // - The count of the number of students in the course.
+            PrintDetails(program);
 
             // Warn user about finalization and wait...
-            Console.Write("Press any key to close this window...");
+            Console.Write("Press any key to continue . . .");
             Console.ReadKey();
         }
 
-        static void PrintStudentDetails(Student student)
+        static void PrintDetails(UProgram program)
         {
             Console.WriteLine();
-            Console.WriteLine("Student information:");
-            Console.WriteLine("Full name: {0} {1}", student.firstName, student.lastName);
-            Console.WriteLine("Full address: {0}, {1}, {2}, {3}, {4}", student.address, student.zipCode, student.city, student.stateOrProvince, student.country);
-            Console.WriteLine("Birthdate: {0}", student.birthdate);
+            Console.WriteLine("The {0} program contains the {1} degree", program.Name, program.Degree.Name);
+            Console.WriteLine();
+            Console.WriteLine("The {0} degree contains the course {1}", program.Degree.Name, program.Degree.Course.Name);
+            Console.WriteLine();
+            Console.WriteLine("The {0} course contains {1} student(s)", program.Degree.Course.Name, Student.Enrolled);
             Console.WriteLine();
         }
 
